3b145175683ca343d297699d9307f0fbbbf653c5
Aida N <nacarinobazaga@gmail.com>
2021-04-28 18:45:18 +0200

    feat: display animated gifs using animate.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,10 +7,9 @@ export const GifGrid = ({ category }) => {
 
     const { data:images, loading } = useFetchGif( category );
 
-    console.log(loading);
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className='animate__animated animate__flash'>{category}</h3>
       {loading && <p>Loading...</p>}
       <div className="card-grid">
         {images.map((img) => (
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 export const GifGridItem = ( { id, title, url } ) => {
 
     return (
-        <div id={id} className='card'>
+        <div id={id} className='card animate__animated animate__fadeIn'>
             <p>{title}</p>
             <img src={url} alt={title} />
         </div>
